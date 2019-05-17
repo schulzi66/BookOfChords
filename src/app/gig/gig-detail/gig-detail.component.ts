07a15625deb16f79c858a833491a9dbd35dc55df
Marius Schulze <marius.schulze@pmcs.de>
2019-05-17 13:45:10 +0200

    save on order changed

--- src/app/gig/gig-detail/gig-detail.component.ts
+++ src/app/gig/gig-detail/gig-detail.component.ts
@@ -12,66 +12,62 @@ import { User } from './../../models/user.model';
 import { AuthService } from './../../services/auth.service';
 
 @Component({
-  selector: 'app-gig-detail',
-  templateUrl: './gig-detail.component.html',
-  styleUrls: ['./gig-detail.component.scss']
+    selector: 'app-gig-detail',
+    templateUrl: './gig-detail.component.html',
+    styleUrls: ['./gig-detail.component.scss']
 })
 export class GigDetailComponent implements OnInit {
 
-  private _router: Router;
-  private _gigService: GigService;
-  private _location: Location;
-  private _orderChanged: boolean;
-  private _authService: AuthService;
-  private _configurationService: ConfigurationService;
-  private _songService: SongService
+    private _router: Router;
+    private _gigService: GigService;
+    private _location: Location;
+    private _authService: AuthService;
+    private _configurationService: ConfigurationService;
+    private _songService: SongService
 
-  public configuration: Configuration;
-  public gig: Gig;
-  public songs: Song[];
+    public configuration: Configuration;
+    public gig: Gig;
+    public songs: Song[];
 
-  constructor(router: Router, gigService: GigService, location: Location, authService: AuthService, configurationServcie: ConfigurationService, songService: SongService) {
-    this._router = router;
-    this._gigService = gigService;
-    this._location = location;
-    this._configurationService = configurationServcie;
-    this._authService = authService;
-    this._songService = songService;
-  }
-
-  ngOnInit() {
-    this.gig = this._gigService.retrieveSelectedGig();
-    if (!this.gig) {
-      this._router.navigate(['/gigs']);
+    constructor(router: Router, gigService: GigService, location: Location, authService: AuthService, configurationServcie: ConfigurationService, songService: SongService) {
+        this._router = router;
+        this._gigService = gigService;
+        this._location = location;
+        this._configurationService = configurationServcie;
+        this._authService = authService;
+        this._songService = songService;
     }
-    this._authService.user$.subscribe((user: User) => {
-      this._configurationService.loadConfigurationForUser(user.uid).subscribe((configuration: Configuration) => {
-        this.configuration = configuration;
-      })
-    })
-    this._orderChanged = false;
-  }
 
-  public goBack(): void {
-    if (this._orderChanged) {
-      this._gigService.saveGig(this.gig);
+    ngOnInit() {
+        this.gig = this._gigService.retrieveSelectedGig();
+        if (!this.gig) {
+            this._router.navigate(['/gigs']);
+        }
+        this._authService.user$.subscribe((user: User) => {
+            this._configurationService.loadConfigurationForUser(user.uid).subscribe((configuration: Configuration) => {
+                this.configuration = configuration;
+            })
+        })
     }
-    this._location.back();
-  }
 
-  public drop(event: CdkDragDrop<Song>) {
-    this._orderChanged = true;
-    moveItemInArray(this.gig.songs, event.previousIndex, event.currentIndex);
-  }
-  public setSelectedSong(song: Song): void {
-    this._songService.storeSelectedSong(song);
-  }
+    public goBack(): void {
+        this._gigService.saveGig(this.gig);
+        this._location.back();
+    }
 
-  public removeSelectedSong(): void {
-    this._songService.removeSelectedSong();
-  }
+    public drop(event: CdkDragDrop<Song>) {
+        moveItemInArray(this.gig.songs, event.previousIndex, event.currentIndex);
+    }
 
-  public editSelectedSong(): void {
-    this._router.navigate(['/edit-song']);
-  }
+    public setSelectedSong(song: Song): void {
+        this._songService.storeSelectedSong(song);
+    }
+
+    public removeSelectedSong(): void {
+        this._songService.removeSelectedSong();
+    }
+
+    public editSelectedSong(): void {
+        this._router.navigate(['/edit-song']);
+    }
 }
